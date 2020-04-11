5fa8d2d6722934ce86a915a45e2223e83354a23e
Vasyl Korchynskyi <v.korchynskyy@aurocraft.com>
2020-04-11 17:41:07 +0300

    CHORE: finish module 3

--- index.ts
+++ index.ts
@@ -1,1 +1,60 @@
-console.log("hey rx js");
+import {Observable, of, from, concat, fromEvent} from "rxjs";
+import { allBooks, allReaders } from "./data";
+import { ajax } from "rxjs/ajax";
+
+// let allBooksObservable$ = new Observable(subscriber => {
+//
+// 	if(document.title !== "RxBookTracker") {
+// 		subscriber.error("Incorrect page title");
+// 	}
+//
+// 	for (let book of allBooks) {
+// 		subscriber.next(book)
+// 	}
+//
+// 	setTimeout(() => {
+// 		subscriber.complete()
+// 	}, 2000)
+//
+// 	return () => console.log("Perform it!", "allBooksObservable$")
+// })
+//
+// allBooksObservable$.subscribe((book: any) => console.log(book.title))
+
+// let source1$ = of("hello", 10, true, allReaders[0].name);
+// let source2$ = from(allBooks);
+
+let button = document.querySelector("#readerButton");
+
+fromEvent(button, "click")
+	.subscribe(event => {
+		ajax("/api/readers")
+			.subscribe(ajaxResponse => {
+
+				const readers = ajaxResponse.response;
+				const readersList = document.querySelector("#readers");
+
+				for(let reader of readers) {
+					const element = document.createElement("div");
+					element.innerHTML = `<p>${reader.name}</p>`
+
+					readersList.appendChild(element);
+				}
+			})
+	})
+
+// fromEvent(button, "click")
+// 	.subscribe(event => {
+// 		console.log(event)
+//
+// 		const readersList = document.querySelector("#readers");
+//
+// 		for(let reader of allReaders) {
+// 			const element = document.createElement("div");
+// 			element.innerHTML = `<p>${reader.name}</p>`
+//
+// 			readersList.appendChild(element);
+// 		}
+// 	})
+
+
