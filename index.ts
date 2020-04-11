663fe15a63a75a1d0fdc7b5df71c759b65aa3804
Vasyl Korchynskyi <v.korchynskyy@aurocraft.com>
2020-04-11 19:54:57 +0300

    CHORE: end up 5 module

--- index.ts
+++ index.ts
@@ -1,105 +1,44 @@
-import {Observable, of, from, concat, fromEvent, interval} from "rxjs";
-import { allBooks, allReaders } from "./data";
-import { ajax } from "rxjs/ajax";
+import {fromEvent, interval, Observable, of, throwError} from "rxjs";
+import {mergeMap, map, filter, tap, catchError, take, takeUntil} from "rxjs/operators";
+import {ajax} from "rxjs/ajax";
 
-let button = document.querySelector("#readerButton");
-
-fromEvent(button, "click")
-	.subscribe(event => {
-		ajax("/api/readers")
-			.subscribe(ajaxResponse => {
-
-				const readers = ajaxResponse.response;
-				const readersList = document.querySelector("#readers");
-
-				for(let reader of readers) {
-					const element = document.createElement("div");
-					element.innerHTML = `<p>${reader.name}</p>`
-
-					readersList.appendChild(element);
-				}
-			})
-})
-
-/// ----
-/*const books$ = from(allBooks)
-
-books$.subscribe({
-	next(v) {
-		console.log(v)
-	},
-	complete() {
-		console.log("all done")
-	}
-})*/
-/// ----
-/*
-const currentTime$ = new Observable(subscriber => {
-	const timeString = new Date().toLocaleTimeString()
-	subscriber.next(timeString);
-	subscriber.complete();
-})
-
-
-currentTime$.subscribe({
-	next: v => console.log(v)
-})
-
-setTimeout(() => {
-	currentTime$.subscribe({
-		next: v => console.log(v)
-	})
-}, 1000)
-
-setTimeout(() => {
-	currentTime$.subscribe({
-		next: v => console.log(v)
-	})
-}, 2000)*/
-
-
-const timesDiv = document.querySelector("#times");
-const timesButton = document.querySelector("#timesButton");
-
-// const timer$ = interval(1000);\
+const timerButton = document.querySelector("#cancel");
+const timerOutput = document.querySelector("#times");
 
 const timer$ = new Observable(subscriber => {
 	let i = 0;
-	let intervalID = setInterval(() => {
-		subscriber.next(i++)
+
+	let id = setInterval(() => {
+		subscriber.next(i++);
 	}, 1000)
 
 	return () => {
-		console.log("Executing teardown code.")
-		clearInterval(intervalID);
+		console.log("End up!");
+		clearInterval(id);
+	}
+});
+
+const cancelTimer$ = fromEvent(timerButton, "click");
+
+timer$.pipe(
+	take(5)
+).subscribe({
+	next: val => {
+		timerOutput.innerHTML += `${new Date().toLocaleTimeString()} (${val}) <br />`
 	}
 })
 
-const timerSubscription = timer$.subscribe({
-	next(value) {
-		timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br />`
-	},
-	complete() {
-		console.log("Timer done!")
-	}
-})
+function grabByYear (year, log) {
+	return source$ => source$.pipe(
+		filter(book => book.publicationYear < year),
+		tap(filteredBook => log ? console.log("--- filteredBook ----", filteredBook) : null)
+	)
+}
 
-const renderSubscription = timer$.subscribe({
-	next: value => {
-		console.log("[1]", value)
-	}
-})
+ajax("/api/books")
+	.pipe(
+		mergeMap(res => res.response),
+		grabByYear(1950, true)
+	)
+	.subscribe(value => console.log(value))
 
-const renderSubscription2 = timer$.subscribe({
-	next: value => {
-		console.log("[2]",value)
-	}
-})
-
-timerSubscription.add(renderSubscription);
-timerSubscription.add(renderSubscription2);
-
-fromEvent(timesButton, "click")
-	.subscribe(event => {
-		timerSubscription.unsubscribe()
-	})
