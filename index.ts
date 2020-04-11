211aeeee6169644fdecb75d17aea45159a4cbcf7
Vasyl Korchynskyi <v.korchynskyy@aurocraft.com>
2020-04-11 18:48:04 +0300

    CHORE: end up 4 module

--- index.ts
+++ index.ts
@@ -1,28 +1,6 @@
-import {Observable, of, from, concat, fromEvent} from "rxjs";
+import {Observable, of, from, concat, fromEvent, interval} from "rxjs";
 import { allBooks, allReaders } from "./data";
 import { ajax } from "rxjs/ajax";
-
-// let allBooksObservable$ = new Observable(subscriber => {
-//
-// 	if(document.title !== "RxBookTracker") {
-// 		subscriber.error("Incorrect page title");
-// 	}
-//
-// 	for (let book of allBooks) {
-// 		subscriber.next(book)
-// 	}
-//
-// 	setTimeout(() => {
-// 		subscriber.complete()
-// 	}, 2000)
-//
-// 	return () => console.log("Perform it!", "allBooksObservable$")
-// })
-//
-// allBooksObservable$.subscribe((book: any) => console.log(book.title))
-
-// let source1$ = of("hello", 10, true, allReaders[0].name);
-// let source2$ = from(allBooks);
 
 let button = document.querySelector("#readerButton");
 
@@ -41,20 +19,87 @@ fromEvent(button, "click")
 					readersList.appendChild(element);
 				}
 			})
+})
+
+/// ----
+/*const books$ = from(allBooks)
+
+books$.subscribe({
+	next(v) {
+		console.log(v)
+	},
+	complete() {
+		console.log("all done")
+	}
+})*/
+/// ----
+/*
+const currentTime$ = new Observable(subscriber => {
+	const timeString = new Date().toLocaleTimeString()
+	subscriber.next(timeString);
+	subscriber.complete();
+})
+
+
+currentTime$.subscribe({
+	next: v => console.log(v)
+})
+
+setTimeout(() => {
+	currentTime$.subscribe({
+		next: v => console.log(v)
 	})
+}, 1000)
 
-// fromEvent(button, "click")
-// 	.subscribe(event => {
-// 		console.log(event)
-//
-// 		const readersList = document.querySelector("#readers");
-//
-// 		for(let reader of allReaders) {
-// 			const element = document.createElement("div");
-// 			element.innerHTML = `<p>${reader.name}</p>`
-//
-// 			readersList.appendChild(element);
-// 		}
-// 	})
+setTimeout(() => {
+	currentTime$.subscribe({
+		next: v => console.log(v)
+	})
+}, 2000)*/
 
 
+const timesDiv = document.querySelector("#times");
+const timesButton = document.querySelector("#timesButton");
+
+// const timer$ = interval(1000);\
+
+const timer$ = new Observable(subscriber => {
+	let i = 0;
+	let intervalID = setInterval(() => {
+		subscriber.next(i++)
+	}, 1000)
+
+	return () => {
+		console.log("Executing teardown code.")
+		clearInterval(intervalID);
+	}
+})
+
+const timerSubscription = timer$.subscribe({
+	next(value) {
+		timesDiv.innerHTML += `${new Date().toLocaleTimeString()} (${value}) <br />`
+	},
+	complete() {
+		console.log("Timer done!")
+	}
+})
+
+const renderSubscription = timer$.subscribe({
+	next: value => {
+		console.log("[1]", value)
+	}
+})
+
+const renderSubscription2 = timer$.subscribe({
+	next: value => {
+		console.log("[2]",value)
+	}
+})
+
+timerSubscription.add(renderSubscription);
+timerSubscription.add(renderSubscription2);
+
+fromEvent(timesButton, "click")
+	.subscribe(event => {
+		timerSubscription.unsubscribe()
+	})
